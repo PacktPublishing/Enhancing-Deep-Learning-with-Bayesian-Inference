e5c5b48236fe1b2911750cf0612e3499c63b9134
marian schneider <marian.schneider@onfido.com>
2022-02-09 22:23:35 +0000

    Satndardise implementations.

--- ch05/pbp_b4tf/main.py
+++ ch05/pbp_b4tf/main.py
@@ -12,56 +12,69 @@ from pbp import PBP
 
 np.random.seed(0)
 tf.random.set_seed(0)
-
+NUM_EPOCHS = 40
 
 def main():
-    print("Getting data..")
-    X_train, y_train, X_test, y_test, x_scaler, y_scaler = get_data()
-    print("Training..")
-    pbp = PBP([50, 50, 1], input_shape=X_train.shape[1])
-    pbp.fit(X_train, y_train, batch_size=8)
-    print("Inference..")
-    m, v, y_test = predict(X_test, pbp, x_scaler, y_scaler, y_test)
+    print("Get data..")
+    X_train_norm, y_train_norm, X_test, y_test, x_scaler, y_scaler = get_data()
+
+    print("Fit..")
+    model = fit(X_train_norm, y_train_norm, n_epochs=NUM_EPOCHS)
+    
+    print("Predict..")
+    m, v, y_test_norm, rmse = predict(model, X_test, y_test, x_scaler, y_scaler)
+
     print("Plot..")
-    plot(X_test, m, v, y_scaler, y_test)
+    plot(X_test, y_test_norm, m, v, y_scaler, NUM_EPOCHS, rmse)
 
 
-def get_data():
+def get_data(normalise_train: bool = True, normalise_test: bool = False):
     X, y = datasets.load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.1, random_state=0
     )
     x_scaler = StandardScaler()
     y_scaler = StandardScaler()
-    X_train = x_scaler.fit_transform(X_train)
-    y_train = y_scaler.fit_transform(y_train.reshape(-1, 1))
+    if normalise_train:
+        X_train = x_scaler.fit_transform(X_train)
+        y_train= y_scaler.fit_transform(y_train.reshape(-1, 1))
+    if normalise_test:
+        X_test = x_scaler.fit_transform(X_test)
+        y_test= y_scaler.fit_transform(y_test.reshape(-1, 1))
     return X_train, y_train, X_test, y_test, x_scaler, y_scaler
 
 
-def predict(X_test, pbp, x_scaler, y_scaler, y_test):
+def fit(X_train, y_train, n_epochs: int = 1):
+    """Fit model with pbp."""
+    pbp = PBP([50, 50, 1], input_shape=X_train.shape[1])
+    pbp.fit(X_train, y_train, batch_size=8, n_epochs=n_epochs)
+    return pbp
+
+
+def predict(pbp, X_test, y_test, x_scaler, y_scaler):
     # normalise data
-    X_test = x_scaler.fit_transform(X_test)
-    y_test = y_scaler.fit_transform(y_test.reshape(-1, 1))
+    X_test_norm = x_scaler.fit_transform(X_test)
+    y_test_norm = y_scaler.fit_transform(y_test.reshape(-1, 1))
     # perform inference
-    m, v = pbp.predict(X_test)
-    # calculate rmse and log-likelihood
+    m, v = pbp.predict(X_test_norm)
+    # calculate rmse
     m_squeezed, v_squeezed = tf.squeeze(m), tf.squeeze(v)
-    rmse = np.sqrt(np.mean((y_test - m_squeezed) ** 2))
+    rmse = np.sqrt(np.mean((y_test_norm - m_squeezed) ** 2))
     print(f"{rmse=}")
+    # calculate log-likelihood
     test_ll = np.mean(
         -0.5 * np.log(2 * math.pi * v_squeezed)
-        - 0.5 * (y_test - m_squeezed) ** 2 / v_squeezed
+        - 0.5 * (y_test_norm - m_squeezed) ** 2 / v_squeezed
     )
     print(f"{test_ll=}")
-    return m, v, y_test
+    return m, v, y_test_norm, rmse
 
 
-
-def plot(X_test, m, v, y_scaler, y_test):
+def plot(X_test, y_test_norm, m, v, y_scaler, num_epochs, rmse):
     id = np.arange(X_test.shape[0])
     plt.figure(figsize=(15, 15))
     plt.plot(
-        id, y_scaler.inverse_transform(y_test), linestyle="", marker=".", label="data"
+        id, y_scaler.inverse_transform(y_test_norm), linestyle="", marker=".", label="data"
     )
     plt.plot(id, y_scaler.inverse_transform(m), alpha=0.5, label="predict mean")
     plt.fill_between(
@@ -72,9 +85,11 @@ def plot(X_test, m, v, y_scaler, y_test):
         label="credible interval",
     )
     plt.xlabel("data id")
-    plt.ylabel("target")
+    plt.ylabel("target: home price")
+    plt.ylim([-10, 60])
+    plt.title(f"Tensorflow, {num_epochs} epochs, rmse: {rmse}")
     plt.legend()
-    plt.savefig(Path(__file__).parent / "pbp_results_tf.png")
+    plt.savefig(Path(__file__).parent / f"pbp_results_tf_{num_epochs:02}.png")
 
 
 if __name__ == "__main__":
--- ch05/pbp_theano/main.py
+++ ch05/pbp_theano/main.py
@@ -12,80 +12,106 @@ import matplotlib.pyplot as plt
 
 os.environ["KERAS_BACKEND"] = "theano"
 np.random.seed(1)
+NUM_EPOCHS = 40
 
 
 def main():
-    print("Getting data..")
-    X_train, y_train, X_test, y_test, x_scaler, y_scaler = get_data()
+    print("Get data..")
+    X_train_norm, y_train_norm, X_test, y_test, x_scaler, y_scaler = get_data(normalise_train=False)
     # X_train_old, y_train_old, X_test_old, y_test_old = get_old_data()
-    print("Training..")
+
+    print("Fit..")
     # set normalise=False because data has been normalised already
-    # squeeze y_train to reduce dims to expected dims for model fitting
-    net = train(X_train, np.squeeze(y_train), normalize=False)
-    print("Inference..")
-    m, v, y_test = predict(net, X_test, y_test)
-    print("Plot..")
-    plot(X_test, m, v, y_scaler, y_test)
+    model = fit(X_train_norm, y_train_norm, normalize=True, n_epochs=NUM_EPOCHS)
+    # model = fit(X_train_old, y_train_old, normalize=True, n_epochs=NUM_EPOCHS)
 
 
-def get_data():
+    print("Predict..")
+    m, v, y_test, rmse = predict(model, X_test, y_test)
+    # m, v, y_test = predict(model, X_test_old, y_test_old)
+
+    # print("Plot..")
+    plot(X_test, y_test, m, v, NUM_EPOCHS, rmse)
+
+
+def get_data(normalise_train: bool = True, normalise_test: bool = False):
     X, y = datasets.load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.1, random_state=0
     )
     x_scaler = StandardScaler()
     y_scaler = StandardScaler()
-    X_train = x_scaler.fit_transform(X_train)
-    y_train = y_scaler.fit_transform(y_train.reshape(-1, 1))
+    if normalise_train:
+        X_train = x_scaler.fit_transform(X_train)
+        y_train= y_scaler.fit_transform(y_train.reshape(-1, 1))
+    if normalise_test:
+        X_test = x_scaler.fit_transform(X_test)
+        y_test= y_scaler.fit_transform(y_test.reshape(-1, 1))
     return X_train, y_train, X_test, y_test, x_scaler, y_scaler
 
 
-def train(X_train, y_train, n_hidden_units: int = 50, normalize: bool=True):
-    # We construct the network with one hidden layer with two-hidden layers
-    # with 50 neurons in each one and normalizing the training features to have
-    # zero mean and unit standard deviation in the trainig set.
-    print(f"{normalize=}")
+def fit(
+    X_train,
+    y_train,
+    n_hidden_units: int = 50,
+    n_epochs: int = 40,
+    normalize: bool = False,
+):
+    # squeeze y_train to reduce dims to expected dims for model fitting
+    y_train= np.squeeze(y_train)
+    # fit the model
     net = PBP_net(
-        X_train, y_train, [n_hidden_units, n_hidden_units], normalize=normalize, n_epochs=40
+        X_train,
+        y_train,
+        [n_hidden_units, n_hidden_units],
+        normalize=normalize,
+        n_epochs=n_epochs,
     )
     return net
 
 
 def predict(net: PBP_net, X_test, y_test):
-    # We make predictions for the test set
+    # perform inference
     m, v, v_noise = net.predict(X_test)
-    # We compute the test RMSE
+    # calculate rmse
     rmse = np.sqrt(np.mean((y_test - m) ** 2))
     print(f"{rmse=}")
-
-    # We compute the test log-likelihood
+    # calculate log-likelihood
     test_ll = np.mean(
+        -0.5 * np.log(2 * math.pi * v)
+        - 0.5 * (y_test - m) ** 2 / v
+    )
+    print(f"{test_ll=}")
+    # calculate log-likelihood with v_noise
+    test_ll_with_vnoise = np.mean(
         -0.5 * np.log(2 * math.pi * (v + v_noise))
         - 0.5 * (y_test - m) ** 2 / (v + v_noise)
     )
-    print(f"{test_ll=}")
+    print(f"{test_ll_with_vnoise=}")
 
-    return m, v, y_test
+    return m, v, y_test, rmse
 
 
-def plot(X_test, m, v, y_scaler, y_test):
+def plot(X_test, y_test, m, v, num_epochs, rmse):
     id = np.arange(X_test.shape[0])
     plt.figure(figsize=(15, 15))
     plt.plot(
-        id, y_scaler.inverse_transform(y_test), linestyle="", marker=".", label="data"
+        id, y_test, linestyle="", marker=".", label="data"
     )
-    plt.plot(id, y_scaler.inverse_transform(m), alpha=0.5, label="predict mean")
+    plt.plot(id, m, alpha=0.5, label="predict mean")
     plt.fill_between(
         id,
-        y_scaler.inverse_transform(m + tf.sqrt(v)).squeeze(),
-        y_scaler.inverse_transform(m - tf.sqrt(v)).squeeze(),
+        m + np.sqrt(v),
+        m - np.sqrt(v),
         alpha=0.5,
         label="credible interval",
     )
     plt.xlabel("data id")
-    plt.ylabel("target")
+    plt.ylabel("target: home price")
+    plt.ylim([-10, 60])
+    plt.title(f"Theano, {num_epochs} epochs, rmse: {rmse}")
     plt.legend()
-    plt.savefig(Path(__file__).parent / "pbp_results_theano.png")
+    plt.savefig(Path(__file__).parent / f"pbp_results_theano_{num_epochs:02}.png")
 
 
 def get_old_data():
@@ -107,7 +133,6 @@ def get_old_data():
     X_test = X[index_test, :]
     y_test = y[index_test]
     return X_train, y_train, X_test, y_test
-
 
 
 if __name__ == "__main__":
